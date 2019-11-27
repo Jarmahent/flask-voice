20832fdff3194da7cdd8ef5697e25208b2b9425f
Kevin Hernandez <kevinhernandez@Kevins-MacBook-Pro-2.local>
2019-11-27 13:00:41 -0500

    updates to converter

--- app/converter/convert.py
+++ app/converter/convert.py
@@ -1,3 +1,15 @@
+import subprocess
+
 
 class Converter():
-    def __init__():
+  def __init__(self):
+    pass
+  
+
+  def convert(self, url, configLoc):
+    command_string = f"a2v -u {url} -c {configLoc}"
+    process = subprocess.Popen(command_string,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return (stdout, stderr)
+
+
