9324674e5d92bbb3283fdc0d248b1b5d5851d8b7
Kevin Hernandez <kevinhernandez@Kevins-MacBook-Pro-2.local>
2019-12-03 10:38:54 -0500

    added convert endpoint

--- app/app.py
+++ app/app.py
@@ -1,13 +1,41 @@
 from flask import Flask,request,render_template,jsonify
+import os
+import urllib.parse
+from converter.convert import Converter
+import datetime
+import json
 
-app = Flask(__name__, static_folder="templates/")
+app = Flask(__name__, static_folder="templates/index/index")
+converter = Converter()
 
 @app.route('/')
 def index():
-    return render_template('index/index/index.html')
+  return render_template('index/index/index.html')
+
+@app.route('/api/convert', methods=["POST"])
+def convert_article():
+  incoming = request.get_json()
+  url = incoming["url"]
+  print(incoming["url"])
+  print("\n")
+  now = datetime.datetime.now().strftime("%d-%Y_%H-%M%S")
+
+  # Move everything in between these lines to a config file (yml)
+  # ---------------
+  config_location = "app/config.json"
+  project_id = "ttsapi-259817"
+  article_folder = \
+    f"/Users/kevinhernandez/Documents/flask-voice/app/templates/index/index/articles/{now}"
+  # ---------------
+
+  if not os.path.exists(article_folder):
+    os.mkdir(article_folder)
+  res = converter.convert(url=urllib.parse.unquote(url),project_id=project_id,config_location=config_location,out_dir=os.path.join(article_folder, "test.mp3"))
+
+  return jsonify({"response": str(res)})
 
 
 
 
 if __name__ == "__main__":
-    app.run(port="8000")
+  app.run(port="8000")
--- app/converter/convert.py
+++ app/converter/convert.py
@@ -6,10 +6,12 @@ class Converter():
     pass
   
 
-  def convert(self, url, configLoc):
-    command_string = f"a2v -u {url} -c {configLoc}"
-    process = subprocess.Popen(command_string,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    return (stdout, stderr)
-
+  def convert(self, url=None, config_location=None, project_id=None, out_dir=None):
+    try:
+      command_string = f"a2v -u {url} -c {config_location} --id {project_id} -o {out_dir} "
+      process = subprocess.Popen(command_string,stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
+      stdout, stderr = process.communicate()
+      return (stdout, stderr)
+    except Exception as e:
+      return str(e)
 
